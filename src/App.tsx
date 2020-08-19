4a603855d2bb5a82c7a28b5acd49923d9a929cfa
ilick888 <ilick888@gmail.com>
2020-08-19 23:37:50 +0900

    型学習

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,87 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+const bool: boolean = true;
+let array = [0, 0, 1, true];
+
+interface NAME {
+  first: string;
+  last: string | null;
+}
+
+const name: NAME = { first: "aaa", last: null };
+
+const func1 = (x: number, y: number): number => {
+  return x + y;
+};
+
+type profile = {
+  age: number;
+};
+
+type login = {
+  username: string;
+};
+
+// インターセクションタイプス
+// タイプを合成する
+type user = profile & login;
+
+const user1: user = {
+  age: 10,
+  username: "foo",
+};
+
+// ユニオンタイプス
+// 複数の型をとる定義ができる
+let value: boolean | number;
+value = true;
+
+// リテラルタイプス
+// Enumぽい
+let company: "Google" | "Facebook" | "Apple";
+company = "Apple";
+
+let num: 12 | 100 | 0;
+num = 12;
+
+// type of
+// タイプ型を一致させる
+let msg: string = "hello";
+let msg2: typeof msg;
+msg2 = "AAA";
+
+let animal = { cat: "small cat" };
+let newAnimal: typeof animal = { cat: "AAA" };
+
+// keyof
+type KEY = {
+  primary: string;
+  secondary: string;
+};
+let key1: keyof KEY;
+
+// Enum
+enum OS {
+  windows,
+  mac,
+  linux,
+}
+
+interface pc {
+  id: number;
+  ostype: OS;
+}
+
+const pc1: pc = { id: 100, ostype: OS.mac };
+const pc2: pc = { id: 10, ostype: OS.windows };
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{name.first}</div>
+        <div>{func1(10, 20)}</div>
       </header>
     </div>
   );
